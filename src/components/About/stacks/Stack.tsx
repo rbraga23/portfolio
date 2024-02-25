7507368936be2a054776e865ace7538e61d83ef5
rafael Braga <rafael.braga966@gmail.com>
2024-02-25 02:46:21 -0300

    fix: diable mobile techstack animation

--- src/components/About/stacks/Stack.tsx
+++ src/components/About/stacks/Stack.tsx
@@ -1,14 +1,15 @@
-import { motion } from "framer-motion";
+import { Variants, motion } from "framer-motion";
 import { useTranslation } from "react-i18next";
 import { IStack } from "./istack.type";
 import { FaCode } from "react-icons/fa";
+import { emptyOnMobile } from "../../../utils/checkMobile";
 
 interface StackProps {
   title: string;
   stack: IStack[];
 }
 
-const animationVariants = {
+const animationVariants = emptyOnMobile({
   hidden: {
     opacity: 0,
     y: 50,
@@ -17,7 +18,7 @@ const animationVariants = {
     opacity: 1,
     y: 0,
   },
-};
+});
 export function Stack({ stack, title }: StackProps) {
   const { t } = useTranslation();
 
@@ -26,7 +27,7 @@ export function Stack({ stack, title }: StackProps) {
       <h1 className="text-2xl font-bold mb-2">{t(title)}</h1>
       <div className="flex flex-row flex-wrap gap-3">
         {stack.map((item: IStack) => (
-          <motion.div key={item.name} variants={animationVariants}>
+          <motion.div key={item.name} variants={animationVariants as Variants}>
             <div className="flex flex-col items-center justify-center p-4 rounded-md max-h-20 min-w-24 bg-zinc-200 dark:bg-slate-800">
               <div className="text-2xl dark:text-white text-black">
                 {item.icon ? (
